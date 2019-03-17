8b4b0fce2b717713c82c1bb5870efdb1dd936b0a
Mathieu Lecarme <mlecarme@bearstech.com>
2019-03-17 17:10:26 +0100

    More ports.

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM bearstech/debian
 
 COPY bin/xhgui-agent /usr/local/bin/xhgui-agent
 
+EXPOSE 8002
 USER nobody
 CMD ["xhgui-agent"]
--- xhgui-agent.go
+++ xhgui-agent.go
@@ -12,11 +12,11 @@ import (
 func main() {
 	listen := os.Getenv("LISTEN")
 	if listen == "" {
-		listen = "127.0.0.1:8080"
+		listen = "127.0.0.1:8002"
 	}
 	mongo := os.Getenv("MONGODB_URL")
 	if mongo == "" {
-		mongo = "mongodb://mongo:27072/xhgui"
+		mongo = "mongodb://mongo:27072/xhprof"
 	}
 	ctx := context.Background()
 	a, err := agent.New(ctx, 100, mongo)
