1b3a548735bb50baad0336dce10ad11ef8605840
Mathieu Lecarme <mlecarme@bearstech.com>
2019-05-04 22:17:52 +0200

    More PHP.

--- contribs/client/web/index.php
+++ contribs/client/web/index.php
@@ -1,4 +1,25 @@
-<?php
+<html>
 
-$name = $_GET['name'] ?: 'world';
-echo "hello $name";
+<head>
+    <title>Demo</title>
+</head>
+
+<body>
+
+    <?php
+
+    $name = $_GET['name'] ?: 'world';
+    echo "<h1>Hello $name</h1>";
+
+    function fibo($n)
+    {
+        return (($n < 2) ? 1 : fibo($n - 1) + fibo($n - 2));
+    }
+
+    $n = $_GET['n'] ?: 10;
+    $f = fibo($n);
+    echo "Fibonacci($n) = $f";
+    ?>
+</body>
+
+</html>
